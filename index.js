e2ff27c26515e3cc5bd46a9148f5ca1b33385929
Rahul Rawat <93177337+RahulRawatji@users.noreply.github.com>
2025-09-25 12:35:29 +0000

    Added about project info

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import gsap from 'gsap';
 import { SplitText } from 'gsap/SplitText';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Lenis from "lenis";
 
 document.addEventListener('DOMContentLoaded', function () {
     gsap.registerPlugin(SplitText);
@@ -21,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentHighlighterWidth = 0;
     let targetHighlighterWidth = 0;
 
-    let isMenuOpen = false;
+    let isMenuOpen = true;
     let isMenuAnimating = false;
 
     const menuLinks = document.querySelectorAll('.menu-links a');
@@ -170,6 +172,8 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     navToggle.addEventListener('click', toggleMenu);
+    
+    toggleMenu();
 
     const menuLinkContainers = document.querySelectorAll('.menu-links');
     
@@ -303,3 +307,70 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const lenis = new Lenis();
+    lenis.on("scroll",ScrollTrigger.update);
+    gsap.ticker.add((time)=>{
+        lenis.raf(time * 1000);
+    })
+    gsap.ticker.lagSmoothing(0);
+
+    document.querySelectorAll(".animate-text").forEach((textElement)=>{
+        textElement.setAttribute('data-text', textElement.textContent.trim());
+
+        ScrollTrigger.create({
+            trigger: textElement,
+            start: "top 50%",
+            end: "bottom 50%",
+            scrub:1,
+            onUpdate: self=>{
+                const clipValue= Math.max(0,100 -self.progress *100);
+                textElement.style.setProperty('--clip-value', `${clipValue}%`);
+            }
+        })
+    });
+
+    ScrollTrigger.create({
+        trigger:'.services',
+        start:"top bottom",
+        end:"top top",
+        scrub:1,
+        onUpdate:self=>{
+            const headers = document.querySelectorAll(".services-header");
+            gsap.set(headers[0], {x: `${100 - self.progress * 100}%`});
+            gsap.set(headers[1], {x: `${-100 + self.progress * 100}%`});
+            gsap.set(headers[2], {x: `${100 - self.progress * 100}%`});
+            
+        }
+    });
+
+    ScrollTrigger.create({
+        trigger:'.services',
+        start:"top top",
+        end:`+=${window.innerHeight * 2}`,
+        pin:true,
+        scrub :1,
+        pinSpacing:false,
+        onUpdate:(self)=>{
+            const headers = document.querySelectorAll(".services-header");
+
+            if(self.progress <= 0.5){
+                const yProgress = self.progress / 0.5;
+                gsap.set(headers[0], { y: `${yProgress * 100}%`});
+                gsap.set(headers[2], {y: `${yProgress * -100}%`});
+            }else{
+                gsap.set(headers[0], { y: "100%"});
+                gsap.set(headers[2],{y: "-100%"});
+
+                const scaleProgress = (self.progress - 0.5) / 0.5;
+                const minScale = window.innerWidth <= 1000 ? 0.3 : 0.1;
+                // const scale = 1 - scaleProgress * (1 - minScale);
+
+                // headers.forEach((header)=>gsap.set(header,{scale: scale}))
+            }
+        }
+    })
+
+})
